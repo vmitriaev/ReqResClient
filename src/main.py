44a0950a70cbb6dc254f0e17357156aae49dcfe5
vmitriaev <vmitriaev@gmail.com>
2019-08-02 18:53:22 +0300

    ReqResClient v0.2.1 -- в модуле main удалена переменная b в первом условии

--- src/main.py
+++ src/main.py
@@ -2,11 +2,10 @@ from src.req import Req
 from src.errors import error
 
 while True:
-    m = input('Type method GET, POST, PUT, PATCH, DELETE, or STOP for exit: ')
+    m = input('Type method (GET, POST, PUT, PATCH, DELETE), or STOP for exit: ')
     if m.lower() in ('get', 'delete'):
         u = input('Endpoint: https://reqres.in/api/')
-        b = '{}'
-        r = Req(m, u, b)
+        r = Req(m, u, '{}')
         print(r.send())
 
     elif m.lower() in ('post', 'put', 'patch'):
