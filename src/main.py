c8afd3523c9dfa28fdb195fcfb7013c19ec83a31
v.mitryaev <vmitriaev@gmail.com>
2019-08-09 21:12:29 +0300

    в модуле main запрос эндпойнта вынесен на уровень запроса метода

--- src/main.py
+++ src/main.py
@@ -8,14 +8,13 @@ with open("config.json") as config_file:
 stoplist = conf['stop_words']
 
 while True:
+    u = input(conf['endpoint_list']['basic_url'] + '/')
     m = input('Type method (GET, POST, PUT, PATCH, DELETE), or STOP for exit: ')
     if m.lower() in ('get', 'delete'):
-        u = input(conf['endpoint_list']['basic_url'] + '/')
         r = Req(m, u)
         print(r.send())
 
     elif m.lower() in ('post', 'put', 'patch'):
-        u = input(conf['endpoint_list']['basic_url'] + '/')
         b = input('Body (JSON):' + '\n' + '''''')
         r = Req(m, u, b)
         print(r.send())
